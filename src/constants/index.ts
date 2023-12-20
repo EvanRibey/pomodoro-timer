c00a41b6d4669e3fd5fe37cd6a9bcb86d01f97c6
Evan Ribey <contact@evanribey.com>
2023-12-19 23:07:14 -0500

    Fix captialization issue with label

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,7 +11,7 @@ export const POMO_STATE_DESCRIPTION_BREAK = 'Get up and stretch, maybe grab some
 export const POMO_STATE_DESCRIPTION_BREAK_LONG = 'Step away for a bit, maybe grab a cup of tea or coffee';
 export const POMO_STATE_DESCRIPTION_FOCUS = 'Sit and breathe; you got this';
 
-export const POMO_FORM_LABEL_COUNT = 'Pomodoro sessions';
+export const POMO_FORM_LABEL_COUNT = 'Pomodoro Sessions';
 export const POMO_FORM_LABEL_LONG_CYCLES = 'Use Long Pomodoro Cycles';
 export const POMO_FORM_HELPER_COUNT = 'Every fourth break will be longer';
 export const POMO_FORM_HELPER_LONG_CYCLES = 'A long cycle is 50 mins of focus followed by a 10 min break';
