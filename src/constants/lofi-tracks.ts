6515708cbf72c6d1b5a9cd33ee3758bc2c4c4b1a
Evan Ribey <contact@evanribey.com>
2023-12-20 21:12:47 -0500

    Add all lofi tracks

--- src/constants/lofi-tracks.ts
+++ src/constants/lofi-tracks.ts
@@ -3,6 +3,25 @@ import track2 from '../assets/music/lofi/02 dryhope - somewhere else (Kupla Mast
 import track3 from '../assets/music/lofi/03 Eisu - sheets (Kupla Master) (1).mp3';
 import track4 from '../assets/music/lofi/04 Casiio _ Sleepermane - Phantasm (Kupla Master) (1).mp3';
 import track5 from '../assets/music/lofi/05 Project AER x WYS - after sunset (Kupla Master) (1).mp3';
+import track6 from '../assets/music/lofi/06 Tenno - Luna (Kupla Master) (1).mp3';
+import track7 from '../assets/music/lofi/07 Sebastian Kamae _ Intoku - Lucid (Kupla Master) (1).mp3';
+import track8 from '../assets/music/lofi/08 epektase x j_san - contemplation (Kupla Master) (1).mp3';
+import track9 from '../assets/music/lofi/09 Casiio _ Sleepermane - Signals (Kupla Master) (1).mp3';
+import track10 from '../assets/music/lofi/10 Phlocalyst _ Living Room - Feelings (Kupla Master) (1).mp3';
+import track11 from '../assets/music/lofi/11 Pandrezz - Blind Forest (Kupla Master) (1).mp3';
+import track12 from '../assets/music/lofi/12 SwuM x chief. - Show Me How (Kupla Master) (1).mp3';
+import track13 from '../assets/music/lofi/13 Comodo - Numb (Kupla Master) (1).mp3';
+import track14 from '../assets/music/lofi/14 TABAL x mell-ø - Weightless (Kupla Master) (1).mp3';
+import track15 from '../assets/music/lofi/15 Ambulo - Night Bus (Kupla Master) (1).mp3';
+import track16 from '../assets/music/lofi/16 Kanisan x Frad - Fireflies (Kupla Master) (1).mp3';
+import track17 from '../assets/music/lofi/17 H.1 - Timeless (Kupla Master) (1).mp3';
+import track18 from '../assets/music/lofi/18 Yasumu - Recharge (Kupla Master) (1).mp3';
+import track19 from '../assets/music/lofi/19 cxlt. - fade away (Kupla Master) (1).mp3';
+import track20 from '../assets/music/lofi/20 Mondo Loops x Kanisan - Escape Route (Kupla Master) (1).mp3';
+import track21 from '../assets/music/lofi/21 towerz - fateful slumber (Kupla Master) (1).mp3';
+import track22 from '../assets/music/lofi/22 brillion. - Crescent (Kupla Master) (1).mp3';
+import track23 from '../assets/music/lofi/23 less.people - persist (Kupla Master2).mp3';
+import track24 from '../assets/music/lofi/24 Mondoloops - Drifting Far Away (Kupla Master) (1).mp3';
 
 export const LOFI_PLAYER_TRACKS = [
   {
@@ -34,5 +53,119 @@ export const LOFI_PLAYER_TRACKS = [
     artist: 'Project AER x WYS',
     album: '3 Am. Study Session',
     url: track5,
+  },
+  {
+    artist: 'Tenno',
+    name: 'Luna',
+    album: '3 Am. Study Session',
+    url: track6,
+  },
+  {
+    artist: 'Sebastian Kamae x Intoku',
+    name: 'Lucid',
+    album: '3 Am. Study Session',
+    url: track7,
+  },
+  {
+    artist: 'epektase x j_san',
+    name: 'contemplation',
+    album: '3 Am. Study Session',
+    url: track8,
+  },
+  {
+    artist: 'Casiio x Sleepermane',
+    name: 'Signals',
+    album: '3 Am. Study Session',
+    url: track9,
+  },
+  {
+    artist: 'Phlocalyst x Living Room',
+    name: 'Feelings',
+    album: '3 Am. Study Session',
+    url: track10,
+  },
+  {
+    artist: 'Pandrezz',
+    name: 'Blind Forest',
+    album: '3 Am. Study Session',
+    url: track11,
+  },
+  {
+    artist: 'SwuM x chief.',
+    name: 'Show Me How',
+    album: '3 Am. Study Session',
+    url: track12,
+  },
+  {
+    artist: 'Comodo',
+    name: 'Numb',
+    album: '3 Am. Study Session',
+    url: track13,
+  },
+  {
+    artist: 'TABAL x mell-ø',
+    name: 'Weightless',
+    album: '3 Am. Study Session',
+    url: track14,
+  },
+  {
+    artist: 'Ambulo',
+    name: 'Night Bus',
+    album: '3 Am. Study Session',
+    url: track15,
+  },
+  {
+    artist: 'Kanisan x Frad',
+    name: 'Fireflies',
+    album: '3 Am. Study Session',
+    url: track16,
+  },
+  {
+    artist: 'H.1',
+    name: 'Timeless',
+    album: '3 Am. Study Session',
+    url: track17,
+  },
+  {
+    artist: 'Yasumu',
+    name: 'Recharge',
+    album: '3 Am. Study Session',
+    url: track18,
+  },
+  {
+    artist: 'cxlt.',
+    name: 'fade away',
+    album: '3 Am. Study Session',
+    url: track19,
+  },
+  {
+    artist: 'Mondo Loops x Kanisan',
+    name: 'Escape Route',
+    album: '3 Am. Study Session',
+    url: track20,
+  },
+  {
+    artist: 'towerz',
+    name: 'fateful slumber',
+    album: '3 Am. Study Session',
+    url: track21,
+  },
+  {
+    artist: 'brillion.',
+    name: 'Crescent',
+    album: '3 Am. Study Session',
+    url: track22,
+  },
+  {
+    artist: 'less.people',
+    name: 'persist',
+    album: '3 Am. Study Session',
+    url: track23,
+  },
+  {
+    artist: 'Mondoloops',
+    name: 'Drifting Far Away',
+    album: '3 Am. Study Session',
+    url: track24,
   },
 ];
